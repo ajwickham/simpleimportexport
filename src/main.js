03525c7041061e19a6a252d9a8770acc9c176749
Alan Wickham <ajwickham@yahoo.com>
2021-01-16 12:43:39 +0000

    working version

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import Dog from './scripts.js';
+
+
+let myPuppy = new Dog("Ernie",3);
+let speech = myPuppy.speak();
+
 $(document).ready(function() {
     $("#response").text(speech); 
 });
+
+
--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,11 @@
-function Dog(name,age) {
-    this.name = name;
-    this.age = age;
-}
-debugger
-let myPuppy = new Dog("Ernie",3);
+export default function Dog(name,age) {
+        this.name = name;
+        this.age = age;
+    }
 
 Dog.prototype.speak = function() {
     return "My name is "+this.name+" and I am "+this.age+" years old";
 }
 
-let speech = myPuppy.speak();
+
 
